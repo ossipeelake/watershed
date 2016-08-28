65d334f1244d545230dfcf22da59dacfb7b15ea4
Taylor Smith <mail@taylorsmith.net>
2016-08-28 15:10:17 -0600

    Setup some basic caching

--- app/controllers/elevations_controller.rb
+++ app/controllers/elevations_controller.rb
@@ -1,5 +1,13 @@
 class ElevationsController < ApplicationController
   def index
-    render json: Elevation.all
+    render json: elevations_cache
+  end
+
+  private
+
+  def elevations_cache
+    Rails.cache.fetch('elevations', expires_in: 1.hour) do
+      Elevation.all
+    end
   end
 end
